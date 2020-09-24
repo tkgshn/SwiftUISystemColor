b7e1f1ff0e16aaf8c923279a00a1722b41cf5442
Shunsuke Takagi (MacBook Pro) <takagishunsuke1129@gmail.com>
2020-09-25 04:26:07 +0900

    Initial Commit

---
+++ SwiftUISystemColor/SwiftUISystemColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISystemColorApp.swift
+//  SwiftUISystemColor
+//
+//  Created by Shunsuke Takagi on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISystemColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
